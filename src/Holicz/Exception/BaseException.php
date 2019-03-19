9ab140d21d78bf2e7bc9e783b3feb7b7462f246f
lukasholeczy <info@lukasholeczy.eu>
2019-03-19 11:03:22 +0100

    Initial commit

---
+++ src/Holicz/Exception/BaseException.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Holicz\Exception;
+
+class BaseException extends \Exception
+{
+    private $context;
+
+    public function __construct(ExceptionContext $context)
+    {
+        $this->context = $context;
+
+        parent::__construct($this->getPublicMessage(), $this->getStatusCode());
+    }
+
+    public function getContext(): ExceptionContext
+    {
+        return $this->context;
+    }
+
+    /**
+     * @return string The message that can safely be displayed to end-users because it doesn't contain sensitive data
+     */
+    public function getPublicMessage(): string
+    {
+        return $this->context->getPublicMessage();
+    }
+
+    /**
+     * @return string The full exception message that is logged and it can contain sensitive data
+     */
+    public function getDebugMessage(): ?String
+    {
+        return $this->context->getDebugMessage();
+    }
+
+    public function getParameters(): array
+    {
+        return $this->context->getParameters();
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->context->getStatusCode();
+    }
+}
---
+++ src/Holicz/Exception/ExceptionContext.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Holicz\Exception;
+
+class ExceptionContext
+{
+    private $publicMessage;
+    private $debugMessage;
+    private $parameters;
+    private $statusCode;
+
+    public function __construct(string $publicMessage, ?string $debugMessage = null, array $parameters = [], int $statusCode = 500)
+    {
+        $this->publicMessage = $publicMessage;
+        $this->debugMessage = $debugMessage;
+        $this->parameters = $parameters;
+        $this->statusCode = $statusCode;
+    }
+
+    public function getPublicMessage(): string
+    {
+        return $this->publicMessage;
+    }
+
+    public function getDebugMessage(): ?string
+    {
+        return $this->debugMessage;
+    }
+
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+}
